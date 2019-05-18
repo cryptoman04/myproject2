689b86ba8ce7206b8c501a21bab8242753b1a286
Chris Gonzalez <cryptobank04>
2019-05-18 16:47:01 -0400

    first commit

---
+++ components/Keypad.js
@@ -0,0 +1,32 @@
+// import React from "react";
+// import { View, Text, Dimensions } from 'react-native';
+
+// const { width, height } = Dimensions.get('window');
+
+// const Keypad = props => {
+// 	const { color, onPress } = props;
+// 	const items = [1,2,3,4,5,6,7,8,9,".", 0, "<"];
+
+// 	return (
+// 		<View style={{
+// 			backgroundColor: color,
+// 			width,
+// 			padding: 20,
+// 			height: height / 3,
+// 			flexDirection: 'row',
+// 			flexWrap: 'wrap',
+// 		}}>
+// 			{/* {
+// 				items.map((items, i) => {
+// 					console.log(items)
+// 					return <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+// 						<Text style={{ color: 'white'}}>items[i]</Text>
+// 					</View>
+// 				})
+// 			} */}
+
+// 		</View>
+// 	)
+// }
+
+// export default Keypad;
